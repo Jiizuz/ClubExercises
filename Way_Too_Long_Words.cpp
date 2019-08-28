775587968cff84581379793f2fc7fc6e15489f10
Jiizuz <masterchack92@hotmail.com>
2019-08-28 15:51:19 -0500

    Way Too Long Words

---
+++ Way_Too_Long_Words.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int i;
+    cin>>i;
+    
+    while (i--) {
+        string input;
+        cin >> input;
+        int len = input.length();
+        if (len > 10) cout << input[0] << len-2 << input[len-1] << endl; else cout << input << endl;
+    }
+}
