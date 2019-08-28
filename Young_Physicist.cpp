8f3bea7d540eb7b68bd25adae697cac966c7bd79
Jiizuz <masterchack92@hotmail.com>
2019-08-28 15:48:02 -0500

    Young physicist

---
+++ Young_Physicist.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i, x, y, z, xf = 0, yf = 0, zf = 0;
+    cin >> i;
+    
+    while (i--) {
+    cin >> x >> y >> z;
+        xf += x;
+        yf += y;
+        zf += z;
+    }
+
+    cout << (!xf && !yf&& !zf ? "YES" : "NO") << endl;
+    return 0;
+}
